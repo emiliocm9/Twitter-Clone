6d5b63dd683d542b423288d2d8280c9e4007befe
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-29 01:14:48 -0500

    CREATE view code for form, feed, edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # Devise default url
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Add Rack::LiveReload to the bottom of the middleware stack with the default options:
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 end
